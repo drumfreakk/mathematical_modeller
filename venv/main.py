4e5bbf38f3e95510cb4c0b366f7ef53174658121
Kieran Houtgraaf <kieran.houtgraaf@gmail.com>
2020-02-17 21:07:08 +0100

    Added decrease

--- venv/main.py
+++ venv/main.py
@@ -5,43 +5,51 @@ import math
 def round_up(n, div=0):
     return n + (div - (n%div))
 
+def round_dec(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.ceil(n * multiplier) / multiplier
+
+
 max = 10            # Inhoud emmer, L
 waterniveau = 0.0   # Startwaarde inhoud emmer, L
 toevoer = 0.1       # Watertoevoer, L/s
+r_gat = 0.5          # radius hole, cm
 
-maxIters = 10000.0  # max calculations
-
-sPerT = 0.0001      # precision, steps per second
-#rgat = 2
+maxSec = 400.0      # max seconds
+sPerT = 1      # precision, steps per second
 
 x = []
 y = []
 maxxed = False
-iters = 0.0
+iters = 0.0         # simulated seconds
 
-while not maxxed and iters < maxIters:
+while not maxxed and iters < maxSec:
     x.append(iters)
     y.append(waterniveau)
-    #    uitstroom = 0.19 * rgat ** 2 * math.sqrt(waterniveau)
     waterniveau += toevoer * sPerT
-    #    waterniveau -= uitstroom * sPerT
+    uitstroom = 0.19 * r_gat * r_gat * math.sqrt(waterniveau)
+    waterniveau -= uitstroom * sPerT
     if (waterniveau >= max):
         waterniveau = max
         maxxed = True
     if (waterniveau <= 0):
         waterniveau = 0.0
+    # TODO: add on stable stop
     iters += sPerT
 
-
-x.append(round_up(iters, 50))
-y.append(y[-1])
+if(maxxed):
+    x.append(round_up(iters, 50))
+    y.append(y[-1])
+    axis = [0, round_up(iters, 50), 0, 11]
+else:
+    axis = [0, maxSec, 0, 11]
 
 plt.plot(x, y)
 
 plt.title('Emmer')
 plt.ylabel('waterniveau (L)')
 plt.xlabel('tijd (s)')
-plt.axis([0, round_up(iters, 50), 0, 11])
+plt.axis(axis)
 plt.grid(True)
 
 plt.show()
