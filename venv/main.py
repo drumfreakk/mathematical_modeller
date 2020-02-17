1940cd37303ae87a121dbd1e9777541057982acb
Kieran Houtgraaf <kieran.houtgraaf@gmail.com>
2020-02-17 20:20:26 +0100

    first commit

---
+++ venv/main.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+max = 10            # Inhoud emmer, L
+waterniveau = 0.0   # Startwaarde inhoud emmer, L
+toevoer = 0.1       # Watertoevoer, L/s
+
+maxIters = 10000.0  #Max aantal berekeningen
+tBeforeQuit = 5     # seconden waarin de emmer vol is/overstroomt voordat de grafiek stopt
+
+#rgat = 2
+
+x = []
+y = []
+maxxed = 0
+sPerT = 0.001
+iters = 0.0
+
+while maxxed < (tBeforeQuit / sPerT) and iters < maxIters:
+    x.append(iters)
+    y.append(waterniveau)
+    #    uitstroom = 0.19 * rgat ** 2 * math.sqrt(waterniveau)
+    waterniveau += toevoer * sPerT
+    #    waterniveau -= uitstroom * sPerT
+    if (waterniveau >= max):
+        waterniveau = max
+        maxxed += 1
+    if (waterniveau <= 0):
+        waterniveau = 0.0
+    iters += sPerT
+
+plt.plot(x, y)
+
+plt.title('Emmer')
+plt.ylabel('waterniveau (L)')
+plt.xlabel('tijd (s)')
+
+plt.show()
